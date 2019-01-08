a1114d4caf0e3777ef473d73a58f9d5ecb82e071
Dominic Kramer <DominicDKramer@google.com>
2019-01-08 14:46:29 -0800

    chore: update documentation

--- src/index.ts
+++ src/index.ts
@@ -52,9 +52,14 @@ export type RestifyRequestHandler = (req: any, res: any, next: Function) => any;
  *  running on
  * @property {String} [serviceContext.version] - the version the hosting
  *  application is currently labelled as
- * @property {Boolean} [ignoreEnvironmentCheck] - flag indicating whether or not
- *  to communicate errors to the Stackdriver service even if NODE_ENV is not set
- *  to production
+ * @property {String} [reportMode] - flag indicating whether or not
+ *  to communicate errors to the Stackdriver service.  Possible values are:
+ *  -> 'production' (default)
+ *     -> Only report errors if NODE_ENV is set to "production".
+ *  -> 'always'
+ *     -> Always report errors regardless of the value of NODE_ENV.
+ *  -> 'never'
+ *     -> Never report errors regardless of the value of NODE_ENV.
  */
 
 /**
